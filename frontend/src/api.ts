130fa25236921b2735debfa16a5447dab757596b
Farzona <farzonamirsodikova345@gmail.com>
2026-07-16 14:09:51 +0500

    Обновлен адрес backend для продакшена

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,6 +1,6 @@
 ﻿import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://smt-pcb.onrender.com/api';
 
 export const registerUser = async (name: string, email: string, password: string) => {
   const response = await axios.post(`${API_URL}/auth/register`, { name, email, password });
